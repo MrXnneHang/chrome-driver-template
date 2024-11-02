61480eb31a9beb1a8e4e008e87f11b3332ac0f7b
MrXnneHang <XnneHang@gmail.com>
2024-11-02 10:45:04 +0800

    Apply black formatting

--- webrowser_config.py
+++ webrowser_config.py
@@ -18,27 +18,24 @@ class Wish(object):
     def __init__(self):
         self.config = load_config()
         self.option = webdriver.ChromeOptions()
-        self.option.add_argument(
-            r"user-data-dir=./User Data")  # 浏览器路径
+        self.option.add_argument(r"user-data-dir=./User Data")  # 浏览器路径
 
         # 指定Chrome和ChromeDriver的路径
-        self.chrome_path = self.config['chrome']
-        self.chrome_driver_path = self.config['chrome_driver']
+        self.chrome_path = self.config["chrome"]
+        self.chrome_driver_path = self.config["chrome_driver"]
         self.option.binary_location = self.chrome_path
 
         # 使用Service指定ChromeDriver的路径
         self.service = Service(self.chrome_driver_path)
 
-
     def get_data(self):
         """初始化WebDriver并获取cookies"""
         option = webdriver.ChromeOptions()
-        option.add_argument(
-            r"./User Data")  # 浏览器路径
+        option.add_argument(r"./User Data")  # 浏览器路径
 
         # 指定Chrome和ChromeDriver的路径
-        chrome_path = self.config['chrome']
-        chrome_driver_path = self.config['chrome_driver']
+        chrome_path = self.config["chrome"]
+        chrome_driver_path = self.config["chrome_driver"]
         option.binary_location = chrome_path
 
         # 使用Service指定ChromeDriver的路径
@@ -46,18 +43,16 @@ class Wish(object):
 
         # 初始化driver
         driver = webdriver.Chrome(service=service, options=option)
-        driver.get(self.config['target_url'])
+        driver.get(self.config["target_url"])
         sleep(5)  # 等待页面加载
-
-
 
     def run(self):
         self.get_data()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fps = Wish()
     # 初始化driver
     driver = webdriver.Chrome(service=fps.service, options=fps.option)
-    driver.get(fps.config['target_url'])
+    driver.get(fps.config["target_url"])
     sleep(5)
